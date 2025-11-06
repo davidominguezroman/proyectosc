3b86df7b5a9b28d5212774fff126b84664d6f7cc
davidominguezroman <davidominguezroman@gmail.com>
2025-11-06 20:36:10 +0100

    first program

---
+++ first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() /*A simple program*/
+{
+    int num; /*Define variable called num*/
+    num = 1;
+    printf("I am a simple "); /*use printf() function*/
+    printf("computer.\n");
+    printf("My favourite number is %d because it is first.\n", num);
+    return 0;
+}
