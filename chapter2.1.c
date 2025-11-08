e741c59b1964831f24b9010e5e32d432950f9bd7
davidominguezroman <davidominguezroman@gmail.com>
2025-11-08 20:14:39 +0100

    chapter2

---
+++ chapter2.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    printf("David Domínguez\n");
+    printf("David\nDomínguez\n");
+    printf("David ");
+    printf("Domínguez\n");
+    return 0;
+}
---
+++ chapter2.6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int toes;
+    toes =10;
+
+    printf("%d, %d, %d\n", toes, 2 * toes, toes * toes);
+    return 0;
+}
